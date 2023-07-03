977f745236cfbf980f1a2bd6161cf4eb17a1517b
Vitalii Derda <derdavitaliy@gmail.com>
2023-07-03 18:12:36 +0300

    feat: added hw 1 module 2

---
+++ HW_2_1/Logger.cs
@@ -0,0 +1,72 @@
+﻿namespace HW_2_1;
+
+/// <summary>
+/// Enum of log types.
+/// </summary>
+public enum LogType
+{
+    Info,
+    Warning,
+    Error,
+}
+
+/// <summary>
+/// Logger service.
+/// </summary>
+internal class Logger
+{
+    private static Logger? _instance = null;
+    private static readonly object Padlock = new object();
+    public static Logger Instance
+    {
+        get
+        {
+            lock (Padlock)
+            {
+                return _instance ??= new Logger();
+            }
+        }
+    }
+    public StringBuilder Report { get; }
+
+    private Logger()
+    {
+        Report = new StringBuilder();
+    }
+
+    /// <summary>
+    /// This method displays a color message depending on the type of log.
+    /// </summary>
+    /// <param name="logType">Enum of log types.</param>
+    /// <param name="message">Some message to display.</param>
+    private void PrintLog(LogType logType, string message)
+    {
+        ConsoleColor previousColor = Console.ForegroundColor;
+
+        Console.ForegroundColor = logType switch
+        {
+            LogType.Info => ConsoleColor.White,
+            LogType.Warning => ConsoleColor.Yellow,
+            LogType.Error => ConsoleColor.Red,
+            _ => previousColor,
+        };
+
+        Console.WriteLine(message);
+
+        Console.ForegroundColor = previousColor;
+    }
+
+    /// <summary>
+    /// This method makes a log entry, adds it to the logger report, and displays it in the console.
+    /// </summary>
+    /// <param name="logType">Enum of log types.</param>
+    /// <param name="message">Some message to display.</param>
+    public void Log(LogType logType, string message)
+    {
+        string log = $"{DateTime.Now}: {logType}: {message}";
+
+        Report.AppendLine(log);
+
+        PrintLog(logType, log);
+    }
+}
---
+++ HW_2_1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HW_2_1;
+
+/// <summary>
+/// Startup class.
+/// </summary>
+internal class Program
+{
+    /// <summary>
+    /// Entry point.
+    /// </summary>
+    /// <param name="args">Some string arguments.</param>
+    private static void Main(string[] args)
+    {
+        Starter starter = new Starter();
+        starter.Run();
+
+        Console.ReadKey();
+    }
+}
