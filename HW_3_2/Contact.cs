81be1a2834cdade9d129d51e708f39369b776e27
Vitalii Derda <derdavitaliy@gmail.com>
2023-07-29 18:44:43 +0300

    feat: added hw 2 module 3

---
+++ HW_3_2/Contact.cs
@@ -0,0 +1,17 @@
+﻿namespace HW_3_2;
+
+/// <summary>
+/// Contact Model for the contact record in the phone book.
+/// </summary>
+public sealed class Contact
+{
+    /// <summary>
+    /// Gets or sets the name of the contact.
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the phone number of the contact.
+    /// </summary>
+    public required string PhoneNumber { get; set; }
+}
