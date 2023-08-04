ae694f4cc94d39bcb43e5346a70a2dd1611700f4
Vitalii Derda <derdavitaliy@gmail.com>
2023-08-04 19:10:18 +0300

    feat: add hw 3 module 3

---
+++ HW_3_3/Class1.cs
@@ -0,0 +1,38 @@
+﻿namespace HW_3_3;
+
+/// <summary>
+/// Class 1.
+/// </summary>
+public sealed class Class1
+{
+    /// <summary>
+    /// This delegate refers to the Show method.
+    /// </summary>
+    /// <param name="result">A bool result of dividing without remainder.</param>
+    public delegate void ShowDelegate(bool result);
+
+    /// <summary>
+    /// This field is used to store the reference to the show delegate.
+    /// </summary>
+    private ShowDelegate? _showDelegate;
+
+    /// <summary>
+    /// This method is used to set the show delegate.
+    /// </summary>
+    /// <param name="showDelegate">The show delegate.</param>
+    public void OnShowDelegate(ShowDelegate showDelegate) => _showDelegate = showDelegate;
+
+    /// <summary>
+    /// This method is used to call the show delegate.
+    /// </summary>
+    /// <param name="result">A bool result of dividing without remainder.</param>
+    public void CallShowDelegate(bool result) => _showDelegate?.Invoke(result);
+
+    /// <summary>
+    /// This method is used to multiply two numbers.
+    /// </summary>
+    /// <param name="x">The first number.</param>
+    /// <param name="y">The second number.</param>
+    /// <returns>The result of multiplication.</returns>
+    public int Multiply(int x, int y) => x * y;
+}
---
+++ HW_3_3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace HW_3_3;
+
+/// <summary>
+/// Program entry point.
+/// </summary>
+internal sealed class Program
+{
+    /// <summary>
+    /// Program entry method.
+    /// </summary>
+    /// <param name="args">Some arguments.</param>
+    private static void Main(string[] args)
+    {
+        var class1 = new Class1();
+        var class2 = new Class2();
+
+        var resultDelegate = class2.Calc(class1.Multiply, 6, 2); // 12
+
+        class1.OnShowDelegate(Show);
+        class1.CallShowDelegate(resultDelegate(3)); // 12 % 3 = 0 == 0 => true
+        class1.CallShowDelegate(resultDelegate(5)); // 12 % 5 = 2 == 0 => false
+    }
+
+    /// <summary>
+    /// This method is used to show in the console the result of dividing without remainder.
+    /// </summary>
+    /// <param name="result">A bool result of dividing without remainder.</param>
+    private static void Show(bool result)
+    {
+        Console.WriteLine($"Result: {result}");
+    }
+}
