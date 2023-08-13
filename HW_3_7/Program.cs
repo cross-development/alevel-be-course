e0743a8b9070bcf5891b3e9d73d5b329a68fd7fb
Vitalii Derda <derdavitaliy@gmail.com>
2023-08-13 18:25:12 +0300

    feat: init hw 7 module 3

---
+++ HW_3_7/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HW_3_7;
+
+/// <summary>
+/// Program entry point.
+/// </summary>
+internal class Program
+{
+    /// <summary>
+    /// Program entry method.
+    /// </summary>
+    /// <param name="args">Some arguments.</param>
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
