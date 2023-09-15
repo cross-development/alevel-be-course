877d0b97a24ec3adf02c8b1151bf42d7c39e1331
Vitalii Derda <derdavitaliy@gmail.com>
2023-09-15 18:24:59 +0300

    feat: init hw 6 module 4

---
+++ HW_4_6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HW_4_6;
+
+/// <summary>
+/// Program entry point.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    ///  Program entry method.
+    /// </summary>
+    /// <param name="args">Some arguments.</param>
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
