00056ffe88389a0fc28e52a63805b284251f8e33
Vitalii Derda <derdavitaliy@gmail.com>
2023-10-14 17:46:04 +0300

    feat: added hw 1 module 6

---
+++ HW_6_1/Controllers/CatalogController.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using HW_6_1.Data;
+
+namespace HW_6_1.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CatalogController : ControllerBase
+{
+    [HttpGet]
+    [Produces(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+    public IEnumerable<Product> GetAllProducts()
+    {
+        var products = Enumerable.Range(1, 5)
+            .Select(index => new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = $"Product-{index}",
+                Description = $"Description for the product-{index}",
+                Price = Random.Shared.Next(10, 1000),
+            }).ToList();
+
+        return products;
+    }
+}
